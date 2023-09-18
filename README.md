d4b6a03d6cbcd34cb3ae84e049f5eb5fc4b50751
Virgiliu <virgiliuweb@gmail.com>
2023-09-18 21:00:44 +0700

    Transitioned from jQuery to vanilla JS; updated documentation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # scrollClass.js
 
-A simple jQuery plugin for adding classes to elements when they are scrolled into view. Use it to add custom CSS3 animations on scroll or to invoke callbacks for other purposes. The plugin aims to be easy to use, flexible, and light.
+A simple JavaScript utility for adding classes to elements when they are scrolled into view. Use it to add custom CSS3 animations on scroll or to invoke callbacks. This utility is designed to be user-friendly, flexible, and lightweight.
 
 [View Demo](http://www.virgiliudiaconu.com/work/scroll-class/)
 
@@ -8,11 +8,13 @@ A simple jQuery plugin for adding classes to elements when they are scrolled int
 
 ### Initialization
 
-Pass a class name or the `data-scroll-class` attribute to the selector to initialize the plugin. Elements with the chosen selector will be targeted on scroll.
+Initialize the utility by targeting elements with the `data-scroll-class` attribute. Elements with this attribute will be targeted on scroll.
 
 ```js
-$(document).ready(function() {
-  $('[data-scroll-class]').scrollClass();
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('[data-scroll-class]').forEach(function(element) {
+    new ScrollClass(element);
+  });
 });
 ```
 
@@ -39,17 +41,20 @@ In the example above, `awesome-animation` will be added to the class attribute o
 ### Initialization in JS with basic options set:
 
 ```js
-$(document).ready(function() {
-  $('.example').scrollClass({
-    delay: 100, // set the class after 100 milliseconds delay
-    threshold: 50, // set the class when 50% of the element enters the viewport
-    offsetTop: 80, // the height in pixels of a fixed top navigation bar
-    callback: function () { // run a custom function when an element enters the viewport
-      console.log('Ahoy there!');
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.example').forEach(function(element) {
+    new ScrollClass(element, {
+      delay: 100, // set the class after 100 milliseconds delay
+      threshold: 50, // set the class when 50% of the element enters the viewport
+      offsetTop: 80, // the height in pixels of a fixed top navigation bar
+      callback: function () { // run a custom function when an element enters the viewport
+        console.log('Element has entered the viewport');
+      }
+    });
   });
 });
 ```
+
 ### Set options in HTML using the `data-scroll` attribute:
 
 Options set with `data-scroll` attributes take precedence over initialization options set in JS. Below is an example with all the available `data-scroll` attributes:
--- scrollClass.js
+++ scrollClass.js
@@ -1,153 +1,116 @@
 /*
- * scrollClass jQuery Plugin v1.2.0
+ * scrollClass JS Plugin v2.0
  *
  * Author: Virgiliu Diaconu
  * http://www.virgiliudiaconu.com
  * Licensed under the MIT license.
  */
-;(function($, window, document, undefined) {
-  'use strict';
-  $.scrollClass = function(el, options) {
-    var base = this;
 
-    // Access to jQuery and DOM versions of element
-    base.$el = $(el);
-    base.el = el;
-
-    // Cached objects
-    base.$win = $(window);
-
-    var viewed = false,
-      timer,
-      throttleDelay;
-
-    // Initialize
-    base.init = function() {
-      base.options = $.extend({}, $.scrollClass.defaultOptions, options);
-
-      // Set options
-      base.delay = base.$el.data("scrollDelay") ?? base.options.delay;
-      base.threshold = base.$el.data("scrollThreshold") ?? base.options.threshold;
-      base.offsetTop = base.$el.data("scrollOffsetTop") ?? base.options.offsetTop;
-      base.reset = base.$el.data("scrollReset") ?? base.options.reset;
-
-      base.onScroll();
-    };
-
-    // Scroll handler
-    base.scrollHandler = function() {
-
-      // Stop execution if the element was viewed and reset is set to false
-      if (viewed === true && base.reset === false) {
-        return;
-      }
-
-      // Init onScroll on load
-      base.onScroll();
-    };
-
-    // On scroll
-    base.onScroll = function() {
-      // Add callback and class when the element enters the viewport
-      if (base.inViewport()) {
-        // Stop execution if the element was viewed and reset is set to true
-        if (viewed === true && base.reset === true) {
-          return;
-        }
-
-        // Callback
-        if (typeof base.options.callback === 'function') {
-          base.options.callback.call(el);
-        }
-
-        // Add class
-        window.clearTimeout(timer);
-        timer = window.setTimeout(function() {
-          base.toggleScrollClass();
-        }, base.delay);
-
-        viewed = true;
-
-        // Remove class and run the reset callback when the element leaves the viewport
-      } else if (base.reset === true) {
-        // Reset callback
-        if (viewed === true && typeof base.options.resetCallback === 'function') {
-          base.options.resetCallback.call(el);
-        }
-
-        // Remove class
-        base.toggleScrollClass();
-
-        viewed = false;
-      }
-    };
-
-    // Add or remove scroll class
-    base.toggleScrollClass = function() {
-      var dataAttr = base.$el.data("scrollClass");
-
-      if (viewed === true) {
-        base.$el.addClass(dataAttr);
-      } else {
-        base.$el.removeClass(dataAttr);
-      }
-    }
-
-    // Check if the element is visible in the viewport
-    base.inViewport = function() {
-      var elRect = base.el.getBoundingClientRect(),
-        winHeight = base.$win.height(),
-        elThreshold = base.threshold;
-
-      // If the window height is smaller than the element height use 50% threshold
-      if (winHeight < elRect.height) {
-        elThreshold = 50;
-      }
-
-      // Convert threshold percent to px of the element's height
-      var thresholdPx = (elThreshold / 100) * elRect.height,
-        thresholdPx = (viewed === false) ? thresholdPx : 1;
-
-      // Return true if the element is in the viewport
-      return (
-        elRect.top + thresholdPx <= winHeight && elRect.bottom - thresholdPx >= base.offsetTop
-      );
-    };
-
-    // Run initializer
-    base.init();
-
-    // Throttle for better performance on scroll
-    function throttle(callback, delay) {
-      if (throttleDelay) {
-        return;
-      }
-
-      throttleDelay = true;
-
-      setTimeout(function() {
-        callback();
-        throttleDelay = false;
-      }, delay);
-    }
-
-    // On scroll listener with throttling
-    base.$win.on('scroll', function() {
-      throttle(base.scrollHandler, base.options.throttle);
-    });
-  };
-
-  $.scrollClass.defaultOptions = {
+class ScrollClass {
+  static defaultOptions = {
     delay: 10,
     threshold: 50,
     reset: false,
     offsetTop: 0,
-    throttle: 50
+    throttle: 50,
   };
 
-  $.fn.scrollClass = function(options) {
-    return this.each(function() {
-      (new $.scrollClass(this, options));
+  constructor(el, options = {}) {
+    this.el = el;
+    this.win = window;
+    this.viewed = false;
+    this.timer = null;
+    this.throttleDelay = null;
+
+    this.options = Object.assign({}, ScrollClass.defaultOptions, options);
+    this.delay = this.el.dataset.scrollDelay ?? this.options.delay;
+    this.threshold = this.el.dataset.scrollThreshold ?? this.options.threshold;
+    this.offsetTop = this.el.dataset.scrollOffsetTop ?? this.options.offsetTop;
+    this.reset = this.el.dataset.scrollReset ?? this.options.reset;
+
+    this.init();
+  }
+
+  init() {
+    this.onScroll();
+    this.win.addEventListener('scroll', () => {
+      this.throttle(this.scrollHandler.bind(this), this.options.throttle);
     });
-  };
-})(jQuery, window, document);
+  }
+
+  scrollHandler() {
+    if (this.viewed === true && this.reset === false) {
+      return;
+    }
+
+    this.onScroll();
+  }
+
+  onScroll() {
+    if (this.inViewport()) {
+      if (this.viewed === true && this.reset === true) {
+        return;
+      }
+
+      if (typeof this.options.callback === 'function') {
+        this.options.callback.call(this.el);
+      }
+
+      window.clearTimeout(this.timer);
+      this.timer = window.setTimeout(() => {
+        this.toggleScrollClass();
+      }, this.delay);
+
+      this.viewed = true;
+    } else if (this.reset === true) {
+      if (this.viewed === true && typeof this.options.resetCallback === 'function') {
+        this.options.resetCallback.call(this.el);
+      }
+
+      this.toggleScrollClass();
+      this.viewed = false;
+    }
+  }
+
+  toggleScrollClass() {
+    const dataAttr = this.el.getAttribute("data-scroll-class");
+
+    if (this.viewed === true) {
+      this.el.classList.add(dataAttr);
+    } else {
+      this.el.classList.remove(dataAttr);
+    }
+  }
+
+  inViewport() {
+    const elRect = this.el.getBoundingClientRect();
+    const winHeight = this.win.innerHeight;
+    let elThreshold = this.threshold;
+
+    if (winHeight < elRect.height) {
+      elThreshold = 50;
+    }
+
+    let thresholdPx = (elThreshold / 100) * elRect.height;
+    thresholdPx = (this.viewed === false) ? thresholdPx : 1;
+
+    return (
+      elRect.top + thresholdPx <= winHeight && elRect.bottom - thresholdPx >= this.offsetTop
+    );
+  }
+
+  throttle(callback, delay) {
+    if (this.throttleDelay) {
+      return;
+    }
+
+    this.throttleDelay = true;
+
+    setTimeout(() => {
+      callback();
+      this.throttleDelay = false;
+    }, delay);
+  }
+}
+
+window.ScrollClass = ScrollClass;
